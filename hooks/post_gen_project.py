3f4dbdd4f76242e665c674b8527e83b94df5d80f
Bruno Alla <browniebroke@users.noreply.github.com>
2020-11-11 23:03:57 +0000

    Post gen hook to run poetry install

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,23 @@
+import subprocess
+
+
+def run_cmd(args, **kwargs):
+    return subprocess.run(args, check=True, **kwargs)
+
+
+def run_poetry_install():
+    try:
+        run_cmd(["poetry", "-h"], capture_output=True)
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        print("poetry command is not installed")
+        return
+
+    run_cmd(["poetry", "install"])
+
+
+def main():
+    run_poetry_install()
+
+
+if __name__ == "__main__":
+    main()
