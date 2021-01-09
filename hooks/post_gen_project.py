f93a7d1d8596c7b81d82b4442be49c43763bd563
Bruno Alla <browniebroke@users.noreply.github.com>
2021-01-09 19:40:22 +0100

    Add more template options

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -21,14 +21,17 @@ def run_poetry_install():
     run_cmd(["poetry", "install"])
 
 
+def initial_commit():
+    # Init local repo
+    run_cmd(["git", "init"])
+    run_cmd(["git", "add", "."])
+    run_cmd(["git", "commit", "-m", "'feat: initial commit'"])
+
+
 def setup_github():
     """Create Github repo and set it up as remote."""
     if not check_command_exists("gh"):
         return
-
-    # Init local repo
-    run_cmd(["git", "init"])
-    run_cmd(["git", "add", "."])
 
     # Create it on Github
     github_username = "{{ cookiecutter.github_username }}"
@@ -47,12 +50,45 @@ def setup_github():
     )
 
 
+def setup_pre_commit():
+    if not check_command_exists("pre-commit"):
+        return
+
+    # Run pre-commit install
+    run_cmd(["pre-commit", "install"])
+
+
+def add_me_as_contributor():
+    if not check_command_exists("npx"):
+        return
+
+    # Run pre-commit install
+    run_cmd(
+        [
+            "npx",
+            "all-contributors-cli",
+            "add",
+            "{{ cookiecutter.github_username }}",
+            "code,ideas,doc",
+        ]
+    )
+
+
 def main():
     if "{{ cookiecutter.run_poetry_install }}" == "y":
         run_poetry_install()
 
+    if "{{ cookiecutter.initial_commit }}" == "y":
+        initial_commit()
+
     if "{{ cookiecutter.setup_github }}" == "y":
         setup_github()
+
+    if "{{ cookiecutter.setup_pre_commit }}" == "y":
+        setup_pre_commit()
+
+    if "{{ cookiecutter.add_me_as_contributor }}" == "y":
+        add_me_as_contributor()
 
 
 if __name__ == "__main__":
--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -3,9 +3,12 @@ from subprocess import CalledProcessError
 import pytest
 
 from hooks.post_gen_project import (
+    add_me_as_contributor,
     check_command_exists,
+    initial_commit,
     run_poetry_install,
     setup_github,
+    setup_pre_commit,
 )
 
 
@@ -35,15 +38,26 @@ def test_run_poetry_install(mocker):
     subprocess_run.assert_any_call(["poetry", "install"], check=True)
 
 
+def test_initial_commit(mocker):
+    subprocess_run = mocker.patch("subprocess.run")
+
+    initial_commit()
+
+    assert subprocess_run.call_count == 3
+    subprocess_run.assert_any_call(["git", "init"], check=True)
+    subprocess_run.assert_any_call(["git", "add", "."], check=True)
+    subprocess_run.assert_any_call(
+        ["git", "commit", "-m", "'feat: initial commit'"], check=True
+    )
+
+
 def test_setup_github(mocker):
     subprocess_run = mocker.patch("subprocess.run")
 
     setup_github()
 
-    assert subprocess_run.call_count == 4
+    assert subprocess_run.call_count == 2
     subprocess_run.assert_any_call(["gh", "-h"], check=True, capture_output=True)
-    subprocess_run.assert_any_call(["git", "init"], check=True)
-    subprocess_run.assert_any_call(["git", "add", "."], check=True)
     subprocess_run.assert_any_call(
         [
             "gh",
@@ -54,6 +68,37 @@ def test_setup_github(mocker):
             "-d",
             "{{ cookiecutter.project_short_description }}",
             "--public",
+        ],
+        check=True,
+    )
+
+
+def test_setup_pre_commit(mocker):
+    subprocess_run = mocker.patch("subprocess.run")
+
+    setup_pre_commit()
+
+    assert subprocess_run.call_count == 2
+    subprocess_run.assert_any_call(
+        ["pre-commit", "-h"], check=True, capture_output=True
+    )
+    subprocess_run.assert_any_call(["pre-commit", "install"], check=True)
+
+
+def test_add_me_as_contributor(mocker):
+    subprocess_run = mocker.patch("subprocess.run")
+
+    add_me_as_contributor()
+
+    assert subprocess_run.call_count == 2
+    subprocess_run.assert_any_call(["npx", "-h"], check=True, capture_output=True)
+    subprocess_run.assert_any_call(
+        [
+            "npx",
+            "all-contributors-cli",
+            "add",
+            "{{ cookiecutter.github_username }}",
+            "code,ideas,doc",
         ],
         check=True,
     )
