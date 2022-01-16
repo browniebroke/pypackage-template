cd810beab603bf2c8de81d069472e57a527a71e3
Bruno Alla <browniebroke@users.noreply.github.com>
2022-01-16 12:53:51 +0000

    fix: initial commit message

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -47,7 +47,8 @@ def test_initial_commit(mocker):
     subprocess_run.assert_any_call(["git", "init"], check=True)
     subprocess_run.assert_any_call(["git", "add", "."], check=True)
     subprocess_run.assert_any_call(
-        ["git", "commit", "-m", "'feat: initial commit'"], check=True
+        ["git", "commit", "-m", "feat: initial commit"],
+        check=True,
     )
 
 
