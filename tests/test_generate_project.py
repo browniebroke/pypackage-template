5b3f929f2ac16e591c67b5c26f4494161770a1b0
Bruno Alla <alla.brunoo@gmail.com>
2022-11-15 23:14:09 +0000

    Updat tests

--- tests/test_generate_project.py
+++ tests/test_generate_project.py
@@ -1,9 +1,10 @@
 import os
-import re
 from pathlib import Path
 
+import copier
 import pytest
-from binaryornot.check import is_binary
+
+PROJECT_ROOT = Path(__file__).parent.parent
 
 
 @pytest.fixture
@@ -19,24 +20,15 @@ def base_context():
     }
 
 
-RE_OBJ = re.compile(r"{{(\s?cookiecutter)[.](.*?)}}")
-
-
-def check_paths(paths):
-    """Method to check all paths have correct substitutions."""
-    # Assert that no match is found in any of the files
-    for path in paths:
-        if is_binary(str(path)):
-            continue
-
-        with path.open() as fd:
-            content = fd.read()
-            match = RE_OBJ.search(content)
-            assert match is None, f"cookiecutter variable not replaced in {path}"
-
-
-def test_generate_project(cookies, base_context):
-    result = cookies.bake(extra_context=base_context)
+def test_generate_project(tmp_path, base_context):
+    worker = copier.run_auto(
+        str(PROJECT_ROOT),
+        tmp_path,
+        data=base_context,
+        defaults=True,
+    )
+    breakpoint()
+    assert worker is not None
 
     assert result.exit_code == 0, result.exception
     assert result.exception is None
