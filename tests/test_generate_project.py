4395de0b8b55035fbff4b3b5b50dc43892908ee3
Bruno Alla <alla.brunoo@gmail.com>
2022-11-15 23:25:16 +0000

    Update tests

--- tests/test_generate_project.py
+++ tests/test_generate_project.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 import copier
@@ -15,8 +14,12 @@ def base_context():
         "github_username": "jdeau",
         "project_name": "Snake Farm",
         "project_short_description": "A sample farming {{ package_name }}",
-        "run_poetry_install": "n",
-        "setup_github": "n",
+        "documentation": True,
+        "run_poetry_install": False,
+        "initial_commit": False,
+        "setup_github": False,
+        "setup_pre_commit": False,
+        "add_me_as_contributor": False,
     }
 
 
@@ -27,43 +30,4 @@ def test_generate_project(tmp_path, base_context):
         data=base_context,
         defaults=True,
     )
-    breakpoint()
     assert worker is not None
-
-    assert result.exit_code == 0, result.exception
-    assert result.exception is None
-    assert result.project_path.name == "snake-farm"
-    assert result.project_path.is_dir()
-
-    paths = [
-        Path(dirpath) / file_path
-        for dirpath, subdirs, files in os.walk(result.project_path)
-        for file_path in files
-    ]
-    assert paths
-    check_paths(paths)
-
-
-@pytest.mark.parametrize(
-    ("docs_opt", "expect_present"),
-    [
-        ("y", True),
-        ("n", False),
-    ],
-)
-def test_generate_documentation_option(cookies, base_context, docs_opt, expect_present):
-    result = cookies.bake(
-        extra_context={
-            **base_context,
-            "documentation": docs_opt,
-        }
-    )
-
-    assert result.exit_code == 0, result.exception
-    root = result.project_path
-    assert (root / "docs").exists() is expect_present
-    assert (root / ".readthedocs.yml").exists() is expect_present
-    assert (
-        "img.shields.io/readthedocs" in (root / "README.md").read_text()
-    ) is expect_present
-    assert ("Sphinx" in (root / "pyproject.toml").read_text()) is expect_present
