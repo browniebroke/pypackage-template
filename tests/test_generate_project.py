fa1f6469db160780176597de4f8a5c67837e7466
Bruno Alla <alla.brunoo@gmail.com>
2025-12-07 18:16:29 +0000

    chore: change tox to install deps from uv.lock

--- tests/test_generate_project.py
+++ tests/test_generate_project.py
@@ -247,6 +247,8 @@ def test_django_package_yes(
             '"django>=4.2"',
             "pytest-django>=4.5,<5",
             "--ds=tests.settings",
+            'django52 = [ "django>=5.2a1,<6" ]',
+            'django42 = [ "django>=4.2a1,<5" ]',
         ],
     )
     _check_file_contents(
@@ -318,10 +320,10 @@ def test_django_package_yes(
     _check_file_contents(
         dst_path / "tox.ini",
         expected_strs=[
-            "django42: Django>=4.2,<5.0",
-            "django50: Django>=5.0,<5.1",
-            "django51: Django>=5.1,<5.2",
-            "django52: Django>=5.2a1,<6.0",
+            "django42: django42",
+            "django50: django50",
+            "django51: django51",
+            "django52: django52",
         ],
     )
     _check_file_contents(
